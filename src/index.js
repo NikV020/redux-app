d01cf7a342a56b10864095b29c3a82dfe0bd7dd8
Nikita <nikitka_ugatu@mail.ru>
2022-11-30 23:34:52 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const initialState = 0;
 
-
-const reducer = (state, action) => {
-  if (action.type === 'INC') {
-    return state + 1;
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state
   }
-  return 0;
 }
 
-let state = reducer(initialState, {type: 'INC'});
+let state = reducer(undefined, {});
+
+state = reducer(state, {type: 'INC'});
 console.log(state);
 state = reducer(state, {type: 'INC'});
 console.log(state);
