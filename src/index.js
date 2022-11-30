d6d919255b0660f4b4b26dba532b1a9bb6477841
Nikita <nikitka_ugatu@mail.ru>
2022-12-01 00:16:48 +0300

    method createStore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
+import {legacy_createStore as createStore} from 'redux';
+
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
     default:
-      return state
+      return state;
   }
 }
 
-let state = reducer(undefined, {});
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+})
 
-state = reducer(state, {type: 'INC'});
-console.log(state);
-state = reducer(state, {type: 'INC'});
-console.log(state);
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+
+
+
+// let state = reducer(undefined, {});
+
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
