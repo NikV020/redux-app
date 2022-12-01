aef22667d7e9de3cb10cb9bd0adb80bf72060ebf
Nikita <nikitka_ugatu@mail.ru>
2022-12-01 23:40:33 +0300

    refactoring code, bindActionCreator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {legacy_createStore as createStore} from 'redux';
+import {legacy_createStore as createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
 import {inc, dec, rnd} from './actions';
 
@@ -6,9 +6,13 @@ import {inc, dec, rnd} from './actions';
 const store = createStore(reducer);
 const {dispatch} = store;
 
-const incDispatch = () => dispatch(inc());
-const decDispatch = () => dispatch(dec());
-const rndDispatch = (value) => dispatch(rnd(value));
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+const incDispatch = bindActionCreators(inc, dispatch);
+const decDispatch = bindActionCreators(dec, dispatch);
+const rndDispatch = bindActionCreators(rnd, dispatch);
 
 document.getElementById('inc').addEventListener('click', incDispatch);
 document.getElementById('dec').addEventListener('click', decDispatch);
