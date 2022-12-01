23238109119178139c6911af10a903d9151e6f52
Nikita <nikitka_ugatu@mail.ru>
2022-12-02 00:03:46 +0300

    cleaning and shortening the code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import {legacy_createStore as createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import {inc, dec, rnd} from './actions';
+import * as actions from './actions';
 
 
 const store = createStore(reducer);
 const {dispatch} = store;
 
+// так работает bindActionCreator, наглядный пример:
 // const bindActionCreator = (creator, dispatch) => (...args) => {
 //   dispatch(creator(...args));
 // }
 
-const incDispatch = bindActionCreators(inc, dispatch);
-const decDispatch = bindActionCreators(dec, dispatch);
-const rndDispatch = bindActionCreators(rnd, dispatch);
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
-document.getElementById('dec').addEventListener('click', decDispatch);
+
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
 document.getElementById('rnd').addEventListener('click', () =>{
   const value = Math.floor(Math.random() * 10);
-  rndDispatch(value);
+  rnd(value);
 });
 
 
